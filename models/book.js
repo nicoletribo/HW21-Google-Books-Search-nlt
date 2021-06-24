9558499130148f85d94dee8c7f2334c2e9f87f63
Nicole Tribo <Nicoletribo@yahoo.com>
2021-06-24 12:07:39 -0500

    created Book schema

--- models/book.js
+++ models/book.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const bookSchema = new Schema({
-    title : String,
-    authors : [String],
-    description : String,
-    image : String,
-    link : String,
+    title: String,
+    authors: [String],
+    description: String,
+    image: String,
+    link: String,
 });
 
 const Book = mongoose.model("Book", bookSchema);
